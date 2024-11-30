e0d2992de74b66c876b9f74ca933d6f022076a52
davidchau21 <davidduongxu1@gmail.com>
2024-11-30 17:48:44 +0700

    qqqq

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react/jsx-runtime'],
+      external: ['react/jsx-runtime', 'react-router-dom'],
     },
   },
 })
