20759316bd2a473e7eefb5b5dbc187c506e83ca8
20114291-TranDinhKhanh <trandinhkhanhan@gmail.com>
2024-12-01 15:23:25 +0700

    minor chat bugs fixed

--- frontend/src/pages/chat.page.jsx
+++ frontend/src/pages/chat.page.jsx
@@ -16,14 +16,14 @@ const ChatUI = () => {
       <section className="gap-2 flex flex-col sm:flex-row h-full sm:h-screen rounded-lg overflow-hidden bg-gray-100 p-4 shadow-lg bg-white relative">
         {/* Sidebar */}
         <div
-          className={`fixed sm:static top-0 left-0 h-full sm:h-auto w-[250px] sm:w-[300px] bg-white shadow-md border border-gray-300 transform ${
-            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-          } sm:translate-x-0 transition-transform duration-300 ${
-            isSidebarOpen ? "z-50" : "sm:z-auto"
-          }`}
-        >
-          <Sidebar />
-        </div>
+        className={`fixed sm:static left-0 top-20 h-full sm:h-auto w-[250px] sm:w-[300px] bg-white transform transition-transform duration-300 rounded-lg ${
+          isSidebarOpen ? "translate-x-0 z-50" : "-translate-x-full sm:translate-x-0 sm:z-auto shadow-md border"
+        } sm:border-gray-300`}
+      >
+        <Sidebar />
+      </div>
+
+
 
         {/* Header (Menu Button + "Đoạn chat") */}
         <div className="sm:hidden flex items-center justify-between">
