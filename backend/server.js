2f662bd07a42cbcbc8cd8def44537e423c0c6b89
davidchau21 <davidduongxu1@gmail.com>
2024-12-19 19:21:31 +0700

    "Added rate limit handler to rate limiter middleware"

--- backend/server.js
+++ backend/server.js
@@ -150,6 +150,11 @@ server.get("/get-upload-url", (req, res) => {
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "Too many requests, please try again later.",
+    });
+  },
 });
 
 server.use(limiter);
